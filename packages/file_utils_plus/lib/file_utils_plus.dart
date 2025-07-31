e73dff6da3bb5b9ae0958a3243a90cd3ee353448
cvbmgh <cvbmgh@126.com>
2025-07-31 19:00:01 +0800

    commit

--- packages/file_utils_plus/lib/file_utils_plus.dart
+++ packages/file_utils_plus/lib/file_utils_plus.dart
@@ -4,6 +4,7 @@ export 'src/file_encoder.dart';
 export 'src/file_operations.dart';
 export 'src/file_reader.dart';
 export 'src/file_type_detector.dart';
+export 'src/file_utils.dart';
 export 'src/file_validator.dart';
 export 'src/file_writer.dart';
 export 'src/temp_file_manager.dart';
