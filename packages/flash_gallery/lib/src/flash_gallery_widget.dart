257d9877597c1659da7ba37332317f905c7559c8
cvbmgh <cvbmgh@126.com>
2025-09-17 19:21:25 +0800

    commit

--- packages/flash_gallery/lib/src/flash_gallery_widget.dart
+++ packages/flash_gallery/lib/src/flash_gallery_widget.dart
@@ -1,9 +1,14 @@
+import 'dart:typed_data';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class FlashGalleryWidget extends StatelessWidget {
   /// 轮播图片列表, asset资源
-  final List<String> assets;
+  final List<String>? assets;
+
+  /// 轮播图片列表, memory资源
+  final List<Uint8List>? memoryImages;
 
   /// 可选宽度
   final double width;
@@ -28,7 +33,8 @@ class FlashGalleryWidget extends StatelessWidget {
 
   const FlashGalleryWidget({
     super.key,
-    required this.assets,
+    this.assets,
+    this.memoryImages,
     this.autoPlayIntervalMillis = 3000,
     this.autoAnimationDurationMillis = 200,
     this.width = 220,
@@ -36,10 +42,12 @@ class FlashGalleryWidget extends StatelessWidget {
     this.radius = 180,
     this.borderColor = Colors.lightBlue,
     this.borderWidth = 2,
-  });
+  }) : assert(assets != null || memoryImages != null,
+            "assets or memoryImages must not be null");
 
   @override
   Widget build(BuildContext context) {
+    var length = assets?.length ?? memoryImages?.length ?? 0;
     return CarouselSlider(
       options: CarouselOptions(
         height: height,
@@ -57,7 +65,34 @@ class FlashGalleryWidget extends StatelessWidget {
         pauseAutoPlayOnTouch: false,
       ),
       // carouselController: _carouselController,
-      items: assets.map((item) {
+      items: List.generate(length, (index) {
+        Widget errorPlaceholder = Container(
+          color: Colors.grey[200],
+          child: Icon(
+            Icons.error_outline,
+            size: 40,
+            color: Colors.grey,
+          ),
+        );
+        Widget widget = SizedBox.shrink();
+        if (assets != null) {
+          widget = Image.asset(
+            assets![index],
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return errorPlaceholder;
+            },
+          );
+        } else if (memoryImages != null) {
+          widget = Image.memory(
+            memoryImages![index],
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return errorPlaceholder;
+            },
+          );
+        }
+
         return Center(
           child: Container(
             width: width,
@@ -71,24 +106,11 @@ class FlashGalleryWidget extends StatelessWidget {
             ),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(radius),
-              child: Image.asset(
-                item,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Container(
-                    color: Colors.grey[200],
-                    child: Icon(
-                      Icons.error_outline,
-                      size: 40,
-                      color: Colors.grey,
-                    ),
-                  );
-                },
-              ),
+              child: widget,
             ),
           ),
         );
-      }).toList(),
+      }),
     );
   }
 }
