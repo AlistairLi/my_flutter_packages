90032d2323827a2ee952c4d1da41b9b41359d903
cvbmgh <cvbmgh@126.com>
2026-05-12 22:45:09 +0800

    Revert "commit"
    This reverts commit 24c73197ec092635e492442e774f57915afe311a.

--- packages/firebase_bootstrap/CHANGELOG.md
+++ packages/firebase_bootstrap/CHANGELOG.md
@@ -4,4 +4,4 @@
 
 ## 1.0.1
 
-- Crashlytics error filter.
+- Filter out the failed image loading due to network anomalies from the fatal reports.
--- packages/firebase_bootstrap/lib/src/firebase_bootstrap_base.dart
+++ packages/firebase_bootstrap/lib/src/firebase_bootstrap_base.dart
@@ -2,8 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 
-import 'crashlytics_error_filter.dart';
-
 /// 初始化 Firebase
 class FirebaseInitializer {
   FirebaseInitializer._();
@@ -13,12 +11,17 @@ class FirebaseInitializer {
       await Firebase.initializeApp();
       FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(!kDebugMode);
 
+      const fatalError = true;
+
       FlutterError.onError = (errorDetails) {
         if (kDebugMode) {
           FlutterError.dumpErrorToConsole(errorDetails);
         } else {
-          if (CrashlyticsErrorFilter.shouldRecordFlutterErrorAsFatal(
-              errorDetails)) {
+          if (_shouldIgnoreFatalFlutterError(errorDetails)) {
+            return;
+          }
+
+          if (fatalError) {
             FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
           } else {
             FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
@@ -31,11 +34,16 @@ class FirebaseInitializer {
             FlutterErrorDetails(exception: error, stack: stack),
           );
         } else {
-          FirebaseCrashlytics.instance.recordError(
-            error,
-            stack,
-            fatal: CrashlyticsErrorFilter.shouldRecordAsFatal(error),
-          );
+          if (_isIgnorableImageLoadException(error, stack)) {
+            return true;
+          }
+
+          if (fatalError) {
+            FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+          } else {
+            // If you want to record a "non-fatal" exception
+            FirebaseCrashlytics.instance.recordError(error, stack);
+          }
         }
         return true;
       };
@@ -45,4 +53,32 @@ class FirebaseInitializer {
       }
     }
   }
+
+  static bool _isIgnorableImageLoadException(Object error, [StackTrace? stackTrace]) {
+    final errorText = error.toString().toLowerCase();
+    final stackText = stackTrace?.toString().toLowerCase() ?? '';
+    final isConnectionClosed =
+    errorText.contains('connection closed while receiving data');
+    final isHttpException = errorText.contains('httpexception');
+    // final isImageRequest = RegExp(
+    //   r'uri\s*=\s*https?:\/\/\S+\.(png|jpg|jpeg|webp|gif|bmp|svg)(\?|$)',
+    //   caseSensitive: false,
+    // ).hasMatch(error.toString());
+    final isImageStack = stackText.contains('cached_network_image') ||
+        stackText.contains('octo_image') ||
+        stackText.contains('flutter_cache_manager') ||
+        stackText.contains('image_provider');
+
+    // return isConnectionClosed &&
+    //     (isHttpException || isImageRequest || isImageStack);
+    return isConnectionClosed && (isHttpException || isImageStack);
+  }
+
+  static bool _shouldIgnoreFatalFlutterError(FlutterErrorDetails errorDetails) {
+    return _isIgnorableImageLoadException(
+      errorDetails.exception,
+      errorDetails.stack,
+    );
+  }
+
 }
